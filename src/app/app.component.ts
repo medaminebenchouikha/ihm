5f23666e01ed4e565f42cbe2174f35fc91af282d
unknown <med.amine.ben.chouikha@gmail.com>
2019-10-09 16:31:04 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component,OnInit  } from '@angular/core';
+import { AppareilService } from './services/appareil.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  appareils: any[];
+
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+ngOnInit() {
+  this.appareils = this.appareilService.appareils;
+}
+  
+  isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+  onAllumer() {
+    this.appareilService.switchOnAll();
+}
+
+onEteindre() {
+  if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
+    this.appareilService.switchOffAll();
+  } else {
+    return null;
+  }
+}
+ 
+}
